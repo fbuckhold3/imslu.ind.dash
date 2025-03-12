4c4d79edd6be8af2ebef82cd833a7fbfc8113be6
fbuckhold3 <78330963+fbuckhold3@users.noreply.github.com>
2025-03-12 13:48:08 -0500

    still trying to fix...

--- R/global.R
+++ R/global.R
@@ -47,7 +47,56 @@ cat("EVAL_TOKEN is set? ", "EVAL_TOKEN" %in% all_env_vars, "\n")
 cat("RDM_TOKEN is set?  ", "RDM_TOKEN"  %in% all_env_vars, "\n")
 cat("FAC_TOKEN is set?  ", "FAC_TOKEN"  %in% all_env_vars, "\n")
 
-
 url <- "https://redcapsurvey.slu.edu/api/"
 
+httr::GET("https://redcapsurvey.slu.edu/api/")
 
+tryCatch({
+  # your API function, e.g. redcapAPI::redcap_read(...)
+}, error = function(e) {
+  cat("API request failed!\n")
+  cat("URL was: ", your_url, "\n")
+  cat("Token length: ", nchar(your_token), "\n")
+  cat("Forms: ", paste(your_forms, collapse=", "), "\n")
+  cat("Error message: ", e$message, "\n")
+})
+
+
+
+# Function to safely pull resident data from REDCap API
+get_resident_data <- function() {
+  tryCatch({
+    ass_dat <- full_api_pull(eval_token, url)
+    ass_dat <- wrangle_assessment_data(ass_dat)
+    rdm_dat <- forms_api_pull(rdm_token, url, 'resident_data', 'faculty_evaluation')
+    return(create_res_data(ass_dat, rdm_dat))
+  }, error = function(e) {
+    cat("Error in API pull:", e$message, "\n")
+    return(NULL)
+  })
+}
+
+# Function to pull and process milestone data
+get_milestone_data <- function() {
+  tryCatch({
+    miles <- get_all_milestones(rdm_token, url)
+    miles <- fill_missing_resident_data(miles)
+    p_miles <- process_milestones(miles, type = "program")
+    s_miles <- process_milestones(miles, type = "self")
+    return(list(p_miles = p_miles, s_miles = s_miles))
+  }, error = function(e) {
+    cat("Error in milestone API pull:", e$message, "\n")
+    return(NULL)
+  })
+}
+
+# Load resident and milestone data
+resident_data <- get_resident_data()
+milestone_data <- get_milestone_data()
+
+p_miles <- milestone_data$p_miles
+s_miles <- milestone_data$s_miles
+
+
+rdm_dict <- get_data_dict(rdm_token, url)
+ass_dict <- get_data_dict(eval_token, url)
